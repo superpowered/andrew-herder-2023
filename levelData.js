7d173c3d754fc8cafc558c04f19f1272f364043d
Andrew Herder <aherder@thiel.com>
2023-04-02 11:43:42 -0500

    Put text bubbles into an array

--- levelData.js
+++ levelData.js
@@ -1,88 +1,87 @@
-export const textBubbleTest = {
-  clearOthers: true,
-  ttl: 6000,
-  removeOnNew: true,
-  removeAllOthers: false,
-  text: [
-    {
-      string: 'Hey!',
-      speed: 10,
-      delayAfter: 500,
-    },
-    {
-      string: 'Woah!',
-      speed: 20,
-      delayAfter: 600,
-    },
-    {
-      string: 'Calm down there bud.',
-      speed: 30,
-    }
-  ],
-};
-
-export const textBubbleTest2 = {
-  clearOthers: true,
-  ttl: 7000,
-  removeOnNew: true,
-  text: [
-    {
-      string: 'No seriously!',
-      speed: 30,
-      delayAfter: 100,
-    },
-    {
-      string: 'I worked hard to place all those pixels',
-      speed: 40,
-      noEndSpace: true,
-      delayAfter: 200,
-    },
-    {
-      string: '...',
-      speed: 400,
-    },
-  ],
-};
-
-export const textBubbleTest3 = {
-  clearOthers: true,
-  ttl: 9000,
-  removeOnNew: true,
-  text: [
-    {
-      string: 'Please stop!',
-      speed: 30,
-      delayAfter: 100,
-    },
-    {
-      string: 'Pretty please?',
-      speed: 40,
-    },
-    {
-      string: 'This is like my life\'s work.',
-      speed: 40,
-    },
-    {
-      string: 'I\'m nothing without this website.',
-      speed: 30,
-      classes: ['small'],
-    },
-  ],
-};
-
-export const textBubbleTest4 = {
-  clearOthers: true,
-  ttl: 2000,
-  removeOnNew: true,
-  classes: ['slam'],
-  text: [
-    {
-      string: 'ENOUGH!',
-      speed: 0,
-      classes: ['bold', 'large'],
-    },
-  ],
-};
+const textBubbles = [
+  {
+    clearOthers: true,
+    ttl: 6000,
+    removeOnNew: true,
+    removeAllOthers: false,
+    text: [
+      {
+        string: 'Hey!',
+        speed: 10,
+        delayAfter: 500,
+      },
+      {
+        string: 'Woah!',
+        speed: 20,
+        delayAfter: 600,
+      },
+      {
+        string: 'Calm down there bud.',
+        speed: 30,
+      }
+    ],
+  },
+  {
+    clearOthers: true,
+    ttl: 7000,
+    removeOnNew: true,
+    text: [
+      {
+        string: 'No seriously!',
+        speed: 30,
+        delayAfter: 100,
+      },
+      {
+        string: 'I worked hard to place all those pixels',
+        speed: 40,
+        noEndSpace: true,
+        delayAfter: 200,
+      },
+      {
+        string: '...',
+        speed: 400,
+      },
+    ],
+  },
+  {
+    clearOthers: true,
+    ttl: 9000,
+    removeOnNew: true,
+    text: [
+      {
+        string: 'Please stop!',
+        speed: 30,
+        delayAfter: 100,
+      },
+      {
+        string: 'Pretty please?',
+        speed: 40,
+      },
+      {
+        string: 'This is like my life\'s work.',
+        speed: 40,
+      },
+      {
+        string: 'I\'m nothing without this website.',
+        speed: 30,
+        classes: ['small'],
+      },
+    ],
+  },
+  {
+    clearOthers: true,
+    ttl: 2000,
+    removeOnNew: true,
+    classes: ['slam'],
+    text: [
+      {
+        string: 'ENOUGH!',
+        speed: 0,
+        classes: ['bold', 'large'],
+      },
+    ],
+  }
+]
 
 export const level0 = {
   events: (levelData) => {
@@ -94,7 +93,7 @@ export const level0 = {
           return levelData.count < levelData.initialCount - levelData.initialCount * .01;
         }  ,
         action: (levelData) => {
-          levelData.game.textSystem.makeText(textBubbleTest);
+          levelData.game.textSystem.makeText(textBubbles[0]);
         }
       },
       {
@@ -104,7 +103,7 @@ export const level0 = {
           return levelData.count < levelData.initialCount - levelData.initialCount * .05;
         }  ,
         action: (levelData) => {
-          levelData.game.textSystem.makeText(textBubbleTest2);
+          levelData.game.textSystem.makeText(textBubbles[1]);
         }
       },
       {
@@ -114,7 +113,7 @@ export const level0 = {
           return levelData.count < levelData.initialCount - levelData.initialCount * .1;
         }  ,
         action: (levelData) => {
-          levelData.game.textSystem.makeText(textBubbleTest3);
+          levelData.game.textSystem.makeText(textBubbles[2]);
         }
       },
       {
@@ -124,7 +123,7 @@ export const level0 = {
           return levelData.count < levelData.initialCount - levelData.initialCount * .15;
         }  ,
         action: (levelData) => {
-          levelData.game.textSystem.makeText(textBubbleTest4);
+          levelData.game.textSystem.makeText(textBubbles[3]);
           levelData.textPixels.forEach( pixel => pixel.destroy(true));
           levelData.textElement.remove();
         }
