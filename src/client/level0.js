2c52a7d531690a536320ef6871bab4f8ac564969
Andrew Herder <andrew.herder@gmail.com>
2023-04-05 23:27:37 -0500

    Optimization to reduce pixel draws. Leaving in commented out code since I'll likely revisit this later

--- src/client/level0.js
+++ src/client/level0.js
@@ -1,4 +1,4 @@
-import { TextPixel } from './textPixel.js';
+import { TextPixel, BigTextPixel } from './textPixel.js';
 import { level0 } from './levelData.js';
 
 // -----------------------------------------------------------------------------
@@ -20,7 +20,10 @@ export class Level0 {
   init = (game, context) => {
     // Create our destructible pixel header
     const pixels = this.createIntroTextPixels(context);
-    this.game.textPixels = pixels.map(pixel => new TextPixel(pixel));
+    const normPixels = pixels.pix.map(pixel => new TextPixel(pixel));
+    const bigPixels = pixels.bigPixels.map(pixel => new BigTextPixel(pixel));
+    // const bigBigPixels = pixels.bigBigPixels.map(pixel => new BigBigTextPixel(pixel));
+    this.game.textPixels = [ ...normPixels, ...bigPixels ];
 
     // Set our counts, which we reference for firing events
     this.initialCount = this.game.textPixels.length;
@@ -38,8 +41,9 @@ export class Level0 {
 
     // Draw out the text
     const introText = this.textElement.textContent;
-    const size = 5 / this.game.dpr;
+    const size = 4 / this.game.dpr;
     context.font = size + "vw 'Press Start 2P'";
+    // context.font = (50  / this.game.dpr) + "px 'Press Start 2P'";
     context.fillStyle="white";
     context.shadowColor="rgb(165,165,165,1)";
     context.shadowOffsetX=3;
@@ -49,7 +53,7 @@ export class Level0 {
     context.textBaseline="middle";
     context.fillText(introText, this.game.width / 2 / this.game.dpr, this.game.height / 2 / this.game.dpr);
 
-    context.font = "1vw 'Courier', sans-serif";
+    context.font = ".75vw 'Courier', sans-serif";
     context.shadowColor="none";
     context.shadowOffsetX=0;
     context.shadowOffsetY=0;
@@ -58,31 +62,188 @@ export class Level0 {
     // This gives us a big crazy array of EVERY pixel on the screen PLUS various data about those pixels 
     // so we store the pixel data for access, but use the Uint32Array to navigate it
     const pixels = context.getImageData(0, 0, this.game.width, this.game.height).data;
-    const data32 = new Uint32Array(pixels.buffer);
+    const data32 = Array.from(new Uint32Array(pixels.buffer));
     let pix = [];
-    for(let i = 0; i < data32.length; i++) {
-      // 0 denotes an empty pixel
-      if (data32[i] === 0) { 
+    let bigPixels = [];
+    let bigBigPixels = [];
+    console.log(pixels.length);
+    // i = data.length; i >= 0; i -= 4
+    // for(let i = pixels.length; i >= 0; i -= 4) {
+    for(let i = 0; i < pixels.length; i += 4) {
+
+      if (pixels[i + 3] <= 0) {
         continue;
       }
 
+      // 0 denotes an empty pixel
+      if (pixels[i] === 0) { 
+        continue;
+      }
+
+      if (pixels[i] === 'beans') { 
+        // console.log('?');
+        continue;
+      }
+
+      // TDOO: we can get pretty good performance gains by reducing the pixel count
+      // reduce by 1.5x 
+      // if (((i / this.game.width)|0) % 8 === 0 ) { 
+      //   continue;
+      // }
+      // if (i % 8 === 0) { 
+      //   continue;
+      // }
+
+      // reduce by 2x 
+      // if (((i / this.game.width)|0) % 4 === 0 ) { 
+      //   continue;
+      // }
+      // if (i % 4 === 0) { 
+      //   continue;
+      // }
+
+      // Reduce by 4x
+      // if (((i / this.game.width)|0) % 2 === 0 ) { 
+      //   continue;
+      // }
+      // if (i % 2 === 0) { 
+      //   continue;
+      // }
+
+      // const neighborPos9 = [
+      //   i, // Middle
+      //   i - this.game.width * 4 - 4, // Upper left
+      //   i - this.game.width * 4, // Upper middle
+      //   i - this.game.width * 4 + 4, // Upper right
+      //   i - 4, // Left
+      //   i + 4, // Right
+      //   i + this.game.width * 4 - 4, // Lower left
+      //   i + this.game.width * 4, // Lower middle
+      //   i + this.game.width * 4 + 4, // Lower right
+      // ];
+
+      // const neighbors9 =  neighborPos9.map((pos) => {
+      //   const hasData = pixels[pos];
+      //   return {
+      //     pos, 
+      //     hasData,
+      //     pixelData: {
+      //       x: ((pos / 4) % this.game.width),
+      //       y: (((pos / 4)  / this.game.width)|0),
+      //       r: pixels[pos], 
+      //       g: pixels[pos + 1], 
+      //       b: pixels[pos + 2],
+      //       a: pixels[pos + 3] / 255
+      //     },
+      // }});
+
+      // const pixelcount9 = neighbors9.reduce((prev, n) => {
+      //   const v = (
+      //     n.pixelData.r === neighbors9[0].pixelData.r && 
+      //     n.pixelData.g === neighbors9[0].pixelData.g && 
+      //     n.pixelData.b === neighbors9[0].pixelData.b && 
+      //     n.pixelData.a === neighbors9[0].pixelData.a
+      //   ) ? 1 : 0;
+      //   return prev + v;
+      // }, 0);
+
+      // if(pixelcount9 === 9) {
+      //   neighbors9.forEach(neighbor => {
+      //     pixels[neighbor.pos] = 'beans';
+      //   });
+      //   bigBigPixels.push(neighbors9);
+      //   continue;
+      //  }
+
+
+      const neighborPos = [
+        i, // TOP LEFT
+        i + 4, // TOP RIGHT
+        i + this.game.width * 4, // BOT LEFT
+        i + this.game.width * 4 + 4, // BOT RIGHT
+      ];
+
+      // const neighborPos2 = [
+      //   i - this.game.width - 4, // Upper left
+      //   i - this.game.width - 3, // Upper middle
+      //   i - this.game.width - 2, // Upper right
+      //   i - this.game.width - 1, // Left
+      //   i, // Middle
+      //   i + this.game.width + 1, // Right
+      //   i + this.game.width + 2, // Lower left
+      //   i + this.game.width + 3, // Lower middle
+      //   i + this.game.width + 4, // Lower right
+      // ];
+
+      const neighbors =  neighborPos.map((pos) => {
+        const hasData = pixels[pos];
+        return {
+          pos, 
+          hasData,
+          pixelData: {
+            x: ((pos / 4) % this.game.width),
+            y: (((pos / 4)  / this.game.width)|0),
+            r: pixels[pos], 
+            g: pixels[pos + 1], 
+            b: pixels[pos + 2],
+            a: pixels[pos + 3] / 255
+          },
+      }});
+
+        //var x = (i / 4) % this.el.width;
+        //var y = Math.floor((i / 4) / this.el.width);
+      // const filtered = neighbors.filter(neighbor => {
+      //   return (
+      //     data32[neighbor.data] !== 0 &&
+      //     neighbor.pixelData.r === neighbors.pixelData && 
+      //     neighbor.pixelData.g === pixels[i*4 + 1] &&
+      //     neighbor.pixelData.b === pixels[i*4 + 2] && 
+      //     neighbor.pixelData.a === pixels[i*4 + 3] / 255
+      //   );
+      // });
+
+      const pixelcount = neighbors.reduce((prev, n) => {
+        const v = (
+          n.pixelData.r === neighbors[0].pixelData.r && 
+          n.pixelData.g === neighbors[0].pixelData.g && 
+          n.pixelData.b === neighbors[0].pixelData.b && 
+          n.pixelData.a === neighbors[0].pixelData.a
+        ) ? 1 : 0;
+        return prev + v;
+      }, 0);
+
+      if(pixelcount === 4) {
+        neighbors.forEach(neighbor => {
+          pixels[neighbor.pos] = 'beans';
+        });
+        bigPixels.push(neighbors);
+        continue;
+       }
+
+      // if(filtered.length === 8) {
+      //   neighbors.forEach(neighbor => {
+      //     data32[neighbor.pos] = 0;
+      //   });
+      //   continue;
+      // }
+
+      // TODO: find neighbors and try to combine into 4x4 rects?
+
       // For every "real" pixel, we'll find the data we need from it and store it here so we can make interactable pixels with them later
-      pix.push({
-        x: (i % this.game.width),
-        // bitwise here is just converting to an int. 32 offset is because of the Uint32Array
-        y: ((i / this.game.width)|0),
-        r: pixels[i*4], 
-        g: pixels[i*4 + 1], 
-        b: pixels[i*4 + 2],
-        a: pixels[i*4 + 3] / 255
-      });
+
+      // console.log({ x: neighbors[4].pixelData.x, y: neighbors[4].pixelData.y });
+
+      pix.push(neighbors[0].pixelData);
     }
 
     // We should be safe to hide the loading text at this point
     this.textElement.classList.add('hidden');
     context.restore();
 
-    return pix;
+    console.log(pix.length);
+    console.log(bigPixels.length);
+    console.log(bigBigPixels.length);
+    return { pix, bigPixels, bigBigPixels };
   }
 
   update(deltaTime){
@@ -99,7 +260,7 @@ export class Level0 {
   // We'll fire this to make sure we've cleaned up anything we may have left behind accidentally
   unload() {
     this.game.textPixels = [];
-    this.textElement.remove();
+    // this.textElement.remove();
     delete this;
   }
 }
--- src/client/textPixel.js
+++ src/client/textPixel.js
@@ -14,7 +14,8 @@ export class TextPixel {
     this.destroyedTime = 0;
     this.markedForDeletion = false;
     this.type = 'textPixel';
-    this.projectileAbsorption = .02;
+    this.projectileAbsorption = .125;
+    // this.projectileAbsorption = 1;
   }
 
   update(deltaTime) {
@@ -32,7 +33,7 @@ export class TextPixel {
 
   draw(context){
     if(this.render) {
-      context.beginPath();
+      //context.beginPath();
       context.fillStyle = `rgba(${this.r}, ${this.b}, ${this.g}, ${this.a})`;
       context.fillRect(this.x, this.y, this.width, this.height);
     }
@@ -53,6 +54,91 @@ export class TextPixel {
 
     for(let x = 0; x < (massDestroy ? 1 : 5); x++) {
       this.particles.push(new TextPixelParticle(this));
+    }
+  }
+}
+
+export class BigTextPixel {
+  constructor(pixels) {
+    // const { x, y, r, g, b, a } = pixel;
+    this.pixels = pixels;
+    this.x = pixels[0].pixelData.x;
+    this.y = pixels[0].pixelData.y;
+    // this.r = r;
+    // this.g = g;
+    // this.b = b;
+    // this.a = a;
+    // this.height = 3;
+    // this.width = 3;
+    this.height = 2;
+    this.width = 2;
+    this.render = true;
+    this.particles = [];
+    this.destroyedTime = 0;
+    this.markedForDeletion = false;
+    this.type = 'textPixel';
+    this.projectileAbsorption = .5;
+    // this.myPixels = pixels.filter(pixel => pixel.hasData && pixel.pixelData.a).map(pixel => new TextPixel(pixel.pixelData));
+    // this.myPixels = pixels.filter(pixel => pixel.hasData && pixel.pixelData.a);
+    // this.myPixels = pixels.filter(pixel => pixel.hasData);
+    this.myPixels = pixels
+  }
+
+  update(deltaTime) {
+    if(this.particles && this.particles.length) {
+      this.particles.forEach(particle => particle.update());
+
+      // Delay destruction
+      this.destroyedTime += deltaTime;
+      if(this.destroyedTime > 500) {
+        this.particles = null;
+        this.markedForDeletion = true;
+      }
+    }
+  }
+
+  draw(context){
+    if(this.render) {
+      context.beginPath();
+      // for(let i = 0; i < this.myPixels.length; i++) {
+      //   context.beginPath();
+      //   // this.myPixels[i].draw(context)
+      //   context.fillStyle = `rgba(${this.myPixels[i].pixelData.r}, ${this.myPixels[i].pixelData.g}, ${this.myPixels[i].pixelData.b}, ${this.myPixels[i].pixelData.a})`;
+      //   context.fillRect(this.myPixels[i].pixelData.x, this.myPixels[i].pixelData.y, 1, 1);
+      //   // context.rect(this.myPixels[i].pixelData.x, this.myPixels[i].pixelData.y, 1, 1);
+      //   // context.strokeStyle = `rgba(${this.myPixels[i].pixelData.r}, ${this.myPixels[i].pixelData.g}, ${this.myPixels[i].pixelData.b}, ${this.myPixels[i].pixelData.a})`;
+      //   // context.stroke();
+      // }
+      // context.beginPath();
+      context.fillStyle = `rgba(${this.myPixels[0].pixelData.r}, ${this.myPixels[0].pixelData.g}, ${this.myPixels[0].pixelData.b}, ${this.myPixels[0].pixelData.a})`;
+      // context.fillStyle = `red`;
+      context.fillRect(this.x, this.y, this.width, this.height);
+      // context.rect(this.x + 1, this.y + 1, this.width - 2, this.height - 2);
+      // context.strokeStyle = 'white';
+      // context.stroke();
+
+      // this.myPixels.map(pixel  => pixel.draw(context));
+      // context.beginPath();
+      // context.fillStyle = `rgba(255, 255, 255, 1)`;
+      // context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    if(this.particles) {
+      this.particles.forEach(particle => particle.draw(context));
+    }
+  }
+
+  hit() {
+    this.destroy();
+  }
+
+  destroy(massDestroy = false) {
+    if(!this.render) {
+      return;
+    }
+    this.render = false;
+
+    for(let x = 0; x < (massDestroy ? 1 : 5); x++) {
+      this.particles.push(new TextPixelParticle(this.myPixels[0].pixelData));
     }
   }
 }
