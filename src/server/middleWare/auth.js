3527661690b2ee9317f6fdc43afc389d8bd48378
Andrew Herder <andrew.herder@gmail.com>
2023-04-23 12:57:49 -0500

    Issue turned out to  be the env variable not being taken. Need  to find a way to rehydrate those on push at some point

--- src/server/middleWare/auth.js
+++ src/server/middleWare/auth.js
@@ -4,18 +4,7 @@ import { IS_PROD_ENV, PUBLIC_URL } from '../constants';
 // -----------------------------------------------------------------------------
 
 const authRequest = (req, res, next) => {
-  const ip = req?.socket?.remoteAddress;
   const origin = req?.get('origin');
-  const host = req?.get('host');
-  const hostname = req?.hostname;
-  console.log({
-    ip,
-    origin,
-    host,
-    hostname,
-    PUBLIC_URL,
-    IS_PROD_ENV,
-  });
 
   if (IS_PROD_ENV && origin !== PUBLIC_URL) {
     return res.status(401).send(`Disallowed`);
